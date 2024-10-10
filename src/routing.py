0eaa3aa7fcf04233a75f40644a66498471e85f6e
David Barron <david.barron91@gmail.com>
2024-10-10 16:59:03 -0500

    chore: add bump route

--- src/routing.py
+++ src/routing.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Request
 from starlette import status
 
 from src.config import get_settings
+from src.config.clients import get_mongo_collection
 
 settings = get_settings()
 
@@ -19,3 +20,18 @@ async def root():
     Healthcheck
     """
     return {"Status": "OK"}
+
+
+@router.get("/bump", status_code=status.HTTP_200_OK)
+def bump():
+    """
+    Bump count in mongo doc
+    """
+    mongo_collection = get_mongo_collection()
+    doc = mongo_collection.find_one({"_id": 1})
+    count = doc["val"]
+    count += 1
+    logger.info(f"Bump count {count}")
+    mongo_collection.update_one({"_id": 1}, {"$set": {"val": count}})
+
+    return {"count": count}
