bffa010b86869151c72a335edfa7f1ef5d12f422
David Barron <davidbarron@Davids-MacBook-Air.local>
2025-05-04 20:52:57 -0500

    chore: trying to get list of set stats

--- src/routing.py
+++ src/routing.py
@@ -1,8 +1,9 @@
+import json
 import logging
 from time import sleep
 
 import redis_lock
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
 from starlette import status
 
 from src.config import get_settings
@@ -13,6 +14,15 @@ settings = get_settings()
 logger = logging.getLogger(settings.app_name)
 
 router: APIRouter = APIRouter()
+
+
+async def get_json(request: Request) -> dict:
+    """
+    Workaround for retrieving a Request body in JSON format in a sync endpoint. FastApi will first set up the Dependency
+    using this async function before sending off the request to be serviced in its own Thread where it can block
+    independently without blocking the event loop which runs the parent application.
+    """
+    return await request.json()
 
 
 @router.get("/", status_code=status.HTTP_200_OK)
@@ -62,3 +72,63 @@ def bump_lock() -> dict:
         mongo_collection.update_one({"_id": 1}, {"$set": {"val": count}})
 
     return {"count": count}
+
+
+@router.post("/", status_code=status.HTTP_204_NO_CONTENT)
+def post_to_redis_set_by_name(
+    json_payload: dict = Depends(get_json),
+):
+    """
+    Post an event to redis set by name
+
+    Format for event is:
+    {
+        "system_id": str,
+        "action": str
+    }
+    """
+    set_name = json_payload["set_name"]
+    event = json_payload["event"]
+
+    redis_client = get_redis_client()
+    redis_client.sadd(set_name, json.dumps(event))
+
+    logger.info(f"Added event to set: {set_name}.", extra={"event": event})
+
+
+@router.get("/{set_name}")
+def get_redis_and_clear_set_by_name(set_name: str):
+    """
+    Get all set contents and clear set to reset
+    """
+    redis_client = get_redis_client()
+    set_members = redis_client.smembers(set_name)
+
+    for member in set_members:
+        redis_client.srem(set_name, member)
+
+    return [json.loads(member) for member in set_members]
+
+
+@router.get("/queue-status", status_code=status.HTTP_200_OK)
+def queue_status() -> dict:
+    """
+    Get name of all sets and count of items in each.
+    """
+    logger.info(f"Get all sets and count of items in each.")
+
+    redis_client = get_redis_client()
+    sets = []
+    response: dict = {}
+
+    for key in redis_client.scan_iter():
+        logger.info(f"Got key: {key}")
+        if redis_client.type(key) == b"set":
+            sets.append(key.decode("utf-8"))
+
+    for set_name in sets:
+        set_members = redis_client.smembers(set_name)
+        response[set_name] = len(set_members)
+
+    return {"blah": 123}
+    # return response
